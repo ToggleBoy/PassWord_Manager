d1e9ca9cd50d962531428ce762322c395e4f3b0c
Radhakrishnan <76608220+ToggleBoy@users.noreply.github.com>
2022-04-01 17:09:24 +0530

    Updated:- Added(Remove Button)

--- main.py
+++ main.py
@@ -106,51 +106,86 @@ def save_data():
 def show_data():
     website = website_entry.get()
 
-    try:
-        with open("Your-Data.json", 'r') as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        messagebox.showwarning(title="Error", message="No Data File Found")
+    if website == "":
+        messagebox.showinfo(title="Caution!!", message="Please don't leave any fields Empty ")
     else:
         try:
-            new_data = data[website]
-        except KeyError:
-            messagebox.showwarning(title="Error",
-                                   message=f"You didn't save the {website} account, yet.\nGo and Save it")
-        # except :
-        #     messagebox.showwarning(title="Error", message="Enter website")
+            with open("Your-Data.json", 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            messagebox.showwarning(title="Error", message="No Data File Found")
         else:
+            try:
+                new_data = data[website]
+            except KeyError:
+                messagebox.showwarning(title="Error",
+                                       message=f"You didn't save the {website} account, yet.\nGo and Save it")
+            # except :
+            #     messagebox.showwarning(title="Error", message="Enter website")
+            else:
 
-            alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-                        't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-            cap_alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-                            "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-                            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+                alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+                cap_alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+                                "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H",
+                                "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y",
+                                "Z"]
 
-            def caesar(start_text, shift_amount):
-                end_text = ""
-                for char in start_text:
-                    if char in alphabet:
-                        position = alphabet.index(char)
-                        new_position = position - shift_amount
-                        end_text += alphabet[new_position]
-                    elif char in cap_alphabet:
-                        position = cap_alphabet.index(char)
-                        new_position = position - shift_amount
-                        end_text += cap_alphabet[new_position]
-                    else:
-                        end_text += char
-                return end_text
+                def caesar(start_text, shift_amount):
+                    end_text = ""
+                    for char in start_text:
+                        if char in alphabet:
+                            position = alphabet.index(char)
+                            new_position = position - shift_amount
+                            end_text += alphabet[new_position]
+                        elif char in cap_alphabet:
+                            position = cap_alphabet.index(char)
+                            new_position = position - shift_amount
+                            end_text += cap_alphabet[new_position]
+                        else:
+                            end_text += char
+                    return end_text
 
-            shift = new_data['ref_id']
-            e_password = new_data["password"]
-            shift = shift % 26
-            d_password = caesar(start_text=e_password, shift_amount=shift)
+                shift = new_data['ref_id']
+                e_password = new_data["password"]
+                shift = shift % 26
+                d_password = caesar(start_text=e_password, shift_amount=shift)
 
-            messagebox.showinfo(title="Your Password", message=f"Website: {website}\n"
-                                                               f"Email: {new_data['email']}\n"
-                                                               f"Password: {d_password}")
+                messagebox.showinfo(title="Your Password", message=f"Website: {website}\n"
+                                                                   f"Email: {new_data['email']}\n"
+                                                                   f"Password: {d_password}")
+
+
+# ------------------------ Remove Password ---------------------------- #
+
+def remove_password():
+    website = website_entry.get()
+
+    if website == "":
+        messagebox.showinfo(title="Caution!!", message="Please don't leave any fields Empty ")
+    else:
+        try:
+            with open("Your-Data.json", 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            messagebox.showwarning(title="Error", message="No Data File Found")
+        else:
+            try:
+                new_data = data[website]
+            except KeyError:
+                messagebox.showwarning(title="Error",
+                                       message=f"You didn't save the {website} account, yet.\nGo and Save it")
+            else:
+                new_data = {key: value for key, value in data.items() if key != website}
+                with open("Your-Data.json", 'w') as file:
+                    json.dump(new_data, file, indent=4)
+                messagebox.showwarning(title="Removed",
+                                       message=f"Your Password for {website} is been Removed.\n")
+
+            finally:
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -198,6 +233,9 @@ password_entry.grid(column=1, row=5)
 empty_label = Label(bg="white")
 empty_label.grid(column=0, row=6)
 
+empty_label = Label(bg="white")
+empty_label.grid(column=0, row=8)
+
 # button
 generate_pass_button = Button(text="Generate Password", borderwidth=0.5)
 generate_pass_button.config(command=generate_password)
@@ -210,4 +248,9 @@ add_button.grid(column=1, row=7, columnspan=2)
 search_button = Button(text="Search", width=14, borderwidth=0.5)
 search_button.config(command=show_data)
 search_button.grid(column=2, row=1)
+
+remove_button = Button(text="Delete Password", width=42, borderwidth=0.5)
+remove_button.config(command=remove_password)
+remove_button.grid(column=1, row=9, columnspan=2)
+
 window.mainloop()
